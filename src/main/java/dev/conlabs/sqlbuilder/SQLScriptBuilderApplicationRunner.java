21d1c11aba94944311aa6aa727bf76980071c9f2
ConLabs: Develop Channel <shud8745@gmail.com>
2021-12-19 12:19:04 +0900

    REPUSH

---
+++ src/main/java/dev/conlabs/sqlbuilder/SQLScriptBuilderApplicationRunner.java
@@ -0,0 +1,16 @@
+package dev.conlabs.sqlbuilder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SQLScriptBuilderApplicationRunner {
+
+	private static Logger logger = LoggerFactory.getLogger(SQLScriptBuilderApplicationRunner.class);
+	
+	public static void main(String[] args) {
+	
+		logger.debug("tester");
+		
+	}
+	
+}
